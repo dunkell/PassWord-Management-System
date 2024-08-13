b5aaa8bc2da1393d5e2c2ba35fe3631f5825d20a
Red.F <1401562765@qq.com>
2024-08-13 11:31:45 +0800

    跨域问题

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 import os
 import sqlite3
 
 app = Flask(__name__)
+CORS(app)
 
 # 配置 SQLite 数据库
 app.config['DATABASE'] = os.path.join(os.getcwd(), 'app', 'database.db')
